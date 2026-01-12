27028f553eb4e7c897c407bd53cd41d21b8c6f40
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2026-01-12 16:24:48 +0000

    Test2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ function Home() {
     <section className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-5xl font-bold mb-4">Hello, I’m Jack.</h1>
       <p className="text-xl text-gray-700">I study computer science at the University of East Anglia and build applications</p>
-      <p className="text-xl text-gray-700">test</p>
       <nav></nav>
     </section>
   );
