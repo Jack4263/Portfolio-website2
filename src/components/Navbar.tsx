80cd8e81f4da941f5d2d07fdc56d6d652b407e34
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2026-01-12 17:29:54 +0000

    Minor tweaks

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: "3rem", borderBottom: "1px #ddd" }}>
+      <a href="#" style={{ marginRight: "1rem" }}>
+        Home
+      </a>
+      <a href="#" style={{ marginRight: "1rem" }}>
+        Projects
+      </a>
+      <a href="#" style={{ marginRight: "1rem" }}>
+        Contact
+      </a>
+    </nav>
+  );
+}
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <section style={{padding: ".5rem 2rem .5rem 2rem", backgroundColor: "#1a1246"}} className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-5xl font-bold mb-4">Hello, I’m Jack.</h1>
       <p className="text-xl text-gray-700">I study computer science at the University of East Anglia and build applications</p>
       <nav></nav>
