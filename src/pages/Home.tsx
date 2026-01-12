bd820c915721ae926b37dbb342e375096bb48287
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2026-01-12 16:20:07 +0000

    Initial React + TypeScript portfolio setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <section className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold mb-4">Hello, I’m Jack.</h1>
+      <p className="text-xl text-gray-700">I study computer science at the University of East Anglia and build applications</p>
+      <nav></nav>
+    </section>
+  );
+}
+
+export default Home;
